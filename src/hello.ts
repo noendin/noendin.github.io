2128ac2114636440945842c775cb68b7f209fe22
Jim Quill <jim.quill@gmail.com>
2016-12-25 02:15:33 +0000

    Fix template definition

--- src/hello.ts
+++ src/hello.ts
@@ -3,12 +3,9 @@ import {Component, Input} from 'angular2/core';
 
 @Component({
   selector: 'hello-world',
-  template: '
-    <div>
-      <h2>Hello {{message}}</h2>
-    </div>
-  '
+  template: '<div>  <h2> Hello {{message}} </h2> </div>'
 })
+
 export class Hello {
 
   @Input()
