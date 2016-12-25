3630b3630bc3fa2ff00e3faac4f3015be852b010
Jim Quill <jim.quill@gmail.com>
2016-12-25 01:08:23 +0000

    Angular 2 Example

---
+++ src/hello.ts
@@ -0,0 +1,17 @@
+//our root app component
+import {Component, Input} from 'angular2/core';
+
+@Component({
+  selector: 'hello-world',
+  template: `
+    <div>
+      <h2>Hello {{message}}</h2>
+    </div>
+  `
+})
+export class Hello {
+
+  @Input()
+  message : String;
+
+}
